4c9cca325f6cb17934246f4933cd65f8d1a65f07
lorsim345 <lorsim@syntoniq.com>
2022-04-14 19:34:16 +0530

    Added initial react setup with network call

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import PokemonList from './Components/PokemonList'
+import axios from 'axios'
+
+function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+   axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p=> p.name))
+  }) 
+  }, [])
+  return (
+    <PokemonList pokemon={pokemon} />
+  );
+}
+
+export default App;
